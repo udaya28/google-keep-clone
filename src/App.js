b5cb2af6f6ef5351e7ed01499047309507c0e96a
udaya28 <54870625+udaya28@users.noreply.github.com>
2020-11-02 20:27:13 +0530

    added firebase successfully

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+// import React, { useState, useEffect } from 'react';
 import './App.scss';
-
+// import firebase,{DB,auth ,signInWithGoogle} from './firebase';
 function App() {
+
   return (
     <div className="App">
-      <h1 className="App">hello google keep</h1>
+      <h1 className="App">Hello Udaya</h1>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+// useEffect(() => {
+//   DB.collection('test')
+//     .onSnapshot((snapshot) => {
+//       console.log(snapshot.docs.map((doc) => doc.data().greet));
+      
+//     });
+    
+// }, []);
---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+const firebaseConfig = {
+  apiKey: 'AIzaSyCMNsOfzm04c2evU5C2BaUHpecMNZ2z7SU',
+  authDomain: 'keep-clone-udaya.firebaseapp.com',
+  databaseURL: 'https://keep-clone-udaya.firebaseio.com',
+  projectId: 'keep-clone-udaya',
+  storageBucket: 'keep-clone-udaya.appspot.com',
+  messagingSenderId: '134603175107',
+  appId: '1:134603175107:web:2a74da86da09ba7ab1de76',
+  measurementId: 'G-BT6KV4XHBC',
+};
+
+const App = firebase.initializeApp(firebaseConfig);
+// const provider = new firebase.auth.GoogleAuthProvider();
+// provider.setCustomParameters({prompt:'select_account'});
+// export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+
+export const DB = App.firestore();
+export const auth = firebase.auth();
+export default firebase;
