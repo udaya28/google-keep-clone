ffa8d264116437d025d58a6db03f94ffee23f662
udaya28 <54870625+udaya28@users.noreply.github.com>
2020-11-05 19:18:04 +0530

    successfully added basic slide on login page still to be finished

--- src/components/LoginPage/loginPage.jsx
+++ src/components/LoginPage/loginPage.jsx
@@ -2,7 +2,9 @@ import Button from '@material-ui/core/Button';
 import './loginPage.scss';
 import { Container } from '@material-ui/core';
 import { signInWithGoogle } from '../../firebase';
-
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import s from '../../svg/xy.svg';
 export const LoginPage = () => {
   const handleSignUp = () => {
     try {
@@ -15,6 +17,38 @@ export const LoginPage = () => {
   return (
     <Container maxWidth="sm">
       <div className="login">
+        <div>
+          <Carousel
+            showArrows={false}
+            showStatus={false}
+            showThumbs={false}
+            infiniteLoop={true}
+            interval={2000}
+            stopOnHover={false}
+            swipeable={true}
+            autoPlay
+          >
+            <div>
+              <img
+                src={s}
+                alt=""
+              />
+              <h1>hello</h1>
+            </div>
+            <div>
+              <img
+                src={s}
+                alt=""
+              />
+            </div>
+            <div>
+              <img
+                src={s}
+                alt=""
+              />
+            </div>
+          </Carousel>
+        </div>
         <Button variant="contained" color="primary" onClick={handleSignUp}>
           SignUp
         </Button>
