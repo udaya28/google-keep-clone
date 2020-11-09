49d14034d769427d3c421a79fab8acae3cb8f8d7
udaya28 <54870625+udaya28@users.noreply.github.com>
2020-11-09 10:34:25 +0530

    formated login page

--- src/components/LoginPage/loginPage.jsx
+++ src/components/LoginPage/loginPage.jsx
@@ -1,7 +1,7 @@
-import Button from '@material-ui/core/Button';
-// import Box from '@material-ui/core/Box';
 import './loginPage.scss';
+import Button from '@material-ui/core/Button';
 import { Container } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import { signInWithGoogle } from '../../firebase';
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
@@ -9,7 +9,21 @@ import todo from '../../svg/todo.svg';
 import notes from '../../svg/notes.svg';
 import sync from '../../svg/sync.svg';
 import googleIcon from '../../svg/google-icon.svg';
-import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    color: '#525252',
+    background: '#ffffff',
+    fontWeight: '700',
+    textTransform: 'none',
+    borderRadius: '20px',
+    width: '80%',
+    marginTop: '15px',
+    '&:hover': {
+      backgroundColor: 'white',
+    },
+  },
+});
 export const LoginPage = () => {
   const handleSignUp = () => {
     try {
@@ -18,24 +32,25 @@ export const LoginPage = () => {
       console.log(error.message);
     }
   };
-  const useStyles = makeStyles({
-    root: {
-      color: '#525252',
-      background: '#ffffff',
-      fontWeight: '700',
-      textTransform: 'none',
-      borderRadius: '20px',
-      width: '80%',
-      marginTop: '15px',
-    },
-  });
+
+  const Slide = (props) => {
+    return (
+      <div>
+        <img src={props.img} alt="" className={props.className} />
+        <div className="content">
+          <p className="heading">{props.head}</p>
+          <p className="caption">{props.content}</p>
+        </div>
+      </div>
+    );
+  };
+
   const classes = useStyles();
   return (
     <div className="full">
       <Container maxWidth="sm" className="login-main">
         <div className="login">
           <h1 className="head">Google keep clone</h1>
-
           <div className="slide-1">
             <Carousel
               showArrows={false}
@@ -47,33 +62,24 @@ export const LoginPage = () => {
               swipeable={true}
               autoPlay
             >
-              <div>
-                <img src={todo} alt="" className="img img-first" />
-                <div className="content">
-                  <p className="heading">Plan your day</p>
-                  <p className="caption">
-                    Stay on track and get things done by making Todo
-                  </p>
-                </div>
-              </div>
-              <div>
-                <img src={notes} alt="" className="img" />
-                <div className="content">
-                  <p className="heading">Stay on track</p>
-                  <p className="caption">
-                    Quickly capture your thoughts anywhere by making Notes
-                  </p>
-                </div>
-              </div>
-              <div>
-                <img src={sync} alt="" className="img img-third" />
-                <div className="content">
-                  <p className="heading">Synchronization in milliseconds </p>
-                  <p className="caption">
-                    Synchronization among sign in devices without reloading
-                  </p>
-                </div>
-              </div>
+              <Slide
+                img={todo}
+                className="img img-first"
+                head="Plan your day"
+                content="Stay on track and get things done by making Todo"
+              />
+              <Slide
+                img={notes}
+                className="img"
+                head="Stay on track"
+                content="Quickly capture your thoughts anywhere by making Notes"
+              />
+              <Slide
+                img={sync}
+                className="img img-third"
+                head="Synchronization in milliseconds"
+                content="Synchronization among sign in devices without reloading"
+              />
             </Carousel>
           </div>
           <Button
@@ -82,7 +88,7 @@ export const LoginPage = () => {
             startIcon={
               <img src={googleIcon} alt="" srcSet="" className="logo-g" />
             }
-            className={classes.root}
+            className={`${classes.root} sign-in-button`}
           >
             Sign in with Google
           </Button>
