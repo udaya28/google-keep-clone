2386a241401652d042b4e5072faf3a42120f47a0
udaya28 <54870625+udaya28@users.noreply.github.com>
2020-11-11 15:24:55 +0530

    test

--- src/components/LandingPage/landingPage.jsx
+++ src/components/LandingPage/landingPage.jsx
@@ -4,11 +4,14 @@ import './landingPage.scss';
 import { Button, Container } from '@material-ui/core';
 export const LandingPage = ({ userData }) => {
   return (
-    <Container maxWidth="sm">
-      <h1>{userData.displayName}</h1>
-      <h2>{userData.email}</h2>
-      <h3>{userData.uid}</h3>
+    <Container >
+      <div className="header">
+      <h1>Hello {userData.displayName}</h1>
+
       <img src={userData.photoURL} alt={userData.displayName} /> <br />
+      </div>
+      {/* <h2>{userData.email}</h2>
+      <h3>{userData.uid}</h3> */}
       <Button
         variant="contained"
         color="primary"
